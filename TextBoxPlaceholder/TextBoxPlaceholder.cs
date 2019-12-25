170dc63f8eb5babef149306b6439cb57ef3d1675
Robert Faust <rob@robfaust.com>
2019-12-25 15:32:40 -0600

    Initial commit

---
+++ TextBoxPlaceholder/TextBoxPlaceholder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Com.RobFaust.Common.UserInterface
+{
+    class TextBoxPlaceholder : TextBox
+    {
+        private string placeholder;
+
+        public TextBoxPlaceholder()
+        {
+            InitializeComponent();
+            Enter += HandleEnter;
+            Leave += HandleLeave;
+            OnLeave(EventArgs.Empty);
+        }
+
+        protected virtual void HandleEnter(object sender, EventArgs e)
+        {
+            if (base.Text.Equals(Placeholder))
+            {
+                base.Text = string.Empty;
+                ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        protected virtual void HandleLeave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Text))
+            {
+                Text = Placeholder;
+                ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        public string Placeholder
+        {
+            get => placeholder;
+            set
+            {
+                placeholder = value;
+                Text = value;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TextBoxPlaceholder
+            // 
+            this.Layout += new System.Windows.Forms.LayoutEventHandler(this.TextBoxPlaceholder_Layout);
+            this.ResumeLayout(false);
+
+        }
+
+        private void TextBoxPlaceholder_Layout(object sender, LayoutEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                Text = Placeholder;
+            }
+        }
+
+        public new string Text
+        {
+            get
+            {
+                if (base.Text.Equals(Placeholder))
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return base.Text;
+                }
+            }
+            set
+            {
+                base.Text = value;
+            }
+        }
+    }
+}
---
+++ TextBoxPlaceholderTest/FrmMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Com.RobFaust.Common.UserInterface
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
